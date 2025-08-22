540da84c4f3291af6dabaf2b6b6ebde5ca41fc70
mudassir733 <deadlool45@gmail.com>
2025-08-22 15:00:39 +0500

    google auth config

---
+++ src/config/passport.ts
@@ -0,0 +1,52 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { prisma } from './db';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+        },
+        async (_accessToken, _refreshToken, profile, done) => {
+            try {
+                const existingUser = await prisma.user.findUnique({
+                    where: {
+                        googleId: profile.id
+                    }
+                });
+                if (existingUser) return done(null, existingUser);
+
+                const newUser = await prisma.user.create({
+                    data: {
+                        googleId: profile.id,
+                        email: profile.emails?.[0].value || '',
+                        username: profile.displayName,
+                        avatar: profile.photos?.[0].value,
+                        password: ''
+                    }
+                });
+
+                return done(null, newUser);
+
+            } catch (error) {
+                return done(error);
+
+            }
+        }
+    )
+);
+
+
+passport.serializeUser((user: any, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id: number, done) => {
+    const user = await prisma.user.findUnique({ where: { id } });
+    done(null, user);
+});
